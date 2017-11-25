562874d05d46053457b095666d117ef35e4ae0e1
Remi Storms <ultimate.spidey.18@gmail.com>
2017-11-25 16:44:43 -0600

    Update

---
+++ Haribo_Soccer_Unity/Assets/_REM_Resources/Scripts/Boot_Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class Boot_Script : MonoBehaviour {
+
+	void Start()
+	{
+		UnityEngine.XR.XRSettings.enabled = false;
+		DontDestroyOnLoad (gameObject);
+	}
+}
---
+++ Haribo_Soccer_Unity/Assets/_REM_Resources/Scripts/GrowButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GrowButton : MonoBehaviour {
+
+	public Vector2 original_scale;
+	public float tween_duration = 0.2f;
+
+	void Start()
+	{
+		original_scale = transform.localScale;
+	}
+
+	public void Grow()
+	{		
+		transform.DOScale (original_scale * 1.1f, tween_duration);
+	}
+
+	public void Shrink()
+	{
+		transform.DOScale (original_scale, tween_duration);
+	}
+}
